7ae1403d6cf54234d1432bf41e0f5f9798e1973d
cxliker <cxliker@qq.com>
2017-10-23 21:59:48 +0800

    start menu

--- electron/assets/scripts/index.js
+++ electron/assets/scripts/index.js
@@ -1,7 +1,22 @@
 var winbt = document.getElementById('winbt');
 winbt.onclick = function () {
-    alert('hello');
+    var win_menu = document.getElementById('win-menu');
+    if (win_menu.className == 'hidden') {
+        win_menu.setAttribute('class', null);
+        document.getElementsByClassName('warp')[0].style.display = 'block';
+    } else {
+        win_menu.setAttribute('class', 'hidden');
+        document.getElementsByClassName('warp')[0].style.display = 'none';
+    }
 }
+
+var warp = document.getElementsByClassName('warp')[0];
+warp.onclick = function () {
+    var win_menu = document.getElementById('win-menu');
+    win_menu.setAttribute('class', 'hidden');
+    this.style.display = 'none';
+}
+
 var filebt = document.getElementById('filebt');
 filebt.onclick = function () {
 
