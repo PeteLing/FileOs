9040e50537b8f2266276bc388c8fb2c1510a7a12
cxliker <cxliker@qq.com>
2017-10-23 20:13:56 +0800

    add a bg and a nav on the bottom

---
+++ electron/assets/scripts/index.js
@@ -0,0 +1,12 @@
+var winbt = document.getElementById('winbt');
+winbt.onclick = function () {
+    alert('hello');
+}
+var filebt = document.getElementById('filebt');
+filebt.onclick = function () {
+
+}
+var cmdbt = document.getElementById('cmdbt');
+cmdbt.onclick = function() {
+
+}
