0a42dfbe9d53e17abef283c3f8e8b00175ef30a4
cxliker <cxliker@qq.com>
2017-10-24 23:21:21 +0800

    file system content background

--- electron/assets/scripts/index.js
+++ electron/assets/scripts/index.js
@@ -25,3 +25,32 @@ var cmdbt = document.getElementById('cmdbt');
 cmdbt.onclick = function() {
 
 }
+
+window.onload = function () {
+
+    function DragDiv(event, id) {
+        var adiv = document.getElementById(id);
+        var intX = event.screenX;
+        var intY = event.clientY;
+        console.log(event.screenX);
+        adiv.ondrag = function (event) {
+            // console.log(event.clientX,intX);
+            adiv.sytle.left = (event.clientX - intX) + 'px';
+            adiv.sytle.top = (event.clientY - intY) + 'px';
+        }(adiv,intX, intY)
+    }
+
+    var fs = document.getElementById('file-system');
+    fs.onmousedown = function (event) {
+        var adiv = document.getElementById('file-system');
+        var intX = event.clientX;
+        var intY = event.clientY;
+        adiv.ondrag = function (e) {
+            // console.log(e);
+            adiv.sytle.left = (e.clientX - intX) + 'px';
+            adiv.sytle.top = (e.clientY - intY) + 'px';
+        }
+    }
+    // fs.ondrag = DragDiv('file-system');
+    // fs.onmouseup = mouseUp();
+  };
